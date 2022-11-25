9729de9d528b28c67cf12d7fc11e43d63a241bb4
Felipe Antonio <96073664+InfoFelipe@users.noreply.github.com>
2022-11-24 23:45:46 -0300

    Formulario, PaginaIncial
    Estruturas FrontEnd na Form e Pagina Inicial

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css"; 
+import PrimeReact from 'primereact/api'; 
 
 export default function App({ Component, pageProps }: AppProps) {
+  PrimeReact.ripple = true;
+  PrimeReact.autoZIndex = true;
+  PrimeReact.appendTo = 'self';
+  PrimeReact.cssTransition = false;
+  PrimeReact.zIndex = {
+    modal: 1100,    // dialog, sidebar
+    overlay: 1000,  // dropdown, overlaypanel
+    menu: 1000,     // overlay menus
+    tooltip: 1100,   // tooltip
+    toast: 1200     // toast
+}
   return <Component {...pageProps} />
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,69 +1,60 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import {Menubar} from 'primereact/menubar'
+import { Card } from 'primereact/card';
+import { Button } from 'primereact/button';
+const Home: NextPage = () =>{
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-      
-      </Head>
+  const itens = [
+    {
+      label: "Home",
+      icon:"pi pi-home"
+    },
+    {
+      label: "Eng. Software",
+      icon:"pi pi-cog"
+    },
+    {
+      label: "Front-End",
+      icon:"pi pi-code"
+    },
+    {
+      label: "Back-End",
+      icon:"pi pi-code"
+    },
+    {
+      label: "Banco de Dados",
+      icon:"pi pi-database"
+    },
+    {
+      label: "Banco de Dados",
+      icon:"pi pi-database"
+    },
+    {
+      label: "Discord",
+      icon:"pi pi-discord px-2"
+    },
+    {
+      label: "Youtube",
+      icon:"pi pi-youtube px-2"
+    },
+    {
+      label: "Instagram",
+      icon:"pi pi-instagram"
+    }
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+  ]
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
+ 
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
+  return(
+    <>
+   
+      <Menubar model={itens}/>
+  <Card style={{padding:'0px', borderRadius:'0px', alignItems:'center', backgroundColor:'#cae6fc'}}>
+    <h1 style={{ position:'relative', marginLeft:'auto'}}>Venha fazer parte do mundo da Tecnologia</h1>
+    <Button>Inscreva-se</Button>
+  </Card>
+    </>
   )
 }
+export default Home
