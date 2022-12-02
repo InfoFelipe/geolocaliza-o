a7114c2d529fee8a0a9cc4efeab9582053f5a241
Felipe Antonio <96073664+InfoFelipe@users.noreply.github.com>
2022-12-02 00:42:18 -0300

    mapa estruturado
    mapa estruturado e organizado

--- pages/Mapas.tsx
+++ pages/Mapas.tsx
@@ -1,5 +1,9 @@
+import { Container } from '@mui/material';
+import Grid from '@mui/material/Grid/Grid';
 import React, { useEffect } from 'react'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import Leaflet from "leaflet";
+import { MarkerLayer } from 'react-leaflet-marker';
 
 
 const Mapeamento = () =>{
@@ -14,21 +18,37 @@ const Mapeamento = () =>{
     }, []);
   }
 
+
   return(
     <>
 <div id='map'>
-
-<MapContainer center={[1.5, -0.09]} zoom={4} scrollWheelZoom={false}>
+<Container>
+<MapContainer center={[51.505, -0.09]} zoom={12} scrollWheelZoom={false} >
   <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
-  <Marker position={[1.5, -0.09]}>
+  <Marker position={[51.505, -0.09]}>
+  <Popup minWidth={90}>
+  
+      </Popup>
+  </Marker>
+
+  <MarkerLayer>
+            <Marker
+                position={[55.796391, 49.108891]}
+            >
+                <div>Hi, i'm a react element</div>
+            </Marker>
+        </MarkerLayer>
+
+  <Marker position={[51.50, -0.09]}>
     <Popup>
       A pretty CSS3 popup. <br /> Easily customizable.
     </Popup>
   </Marker>
 </MapContainer>
+</Container>
 </div>
 
     </>
@@ -36,51 +56,3 @@ const Mapeamento = () =>{
 }
 export default Mapeamento
 
-// import React from 'react'
-// import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
-
-// const containerStyle = {
-//   width: '400px',
-//   height: '400px'
-// };
-
-// const center = {
-//   lat: -3.745,
-//   lng: -38.523
-// };
-
-// function MyComponent() {
-//   const { isLoaded } = useJsApiLoader({
-//     id: 'google-map-script',
-//     googleMapsApiKey: "YOUR_API_KEY"
-//   })
-
-//   const [map, setMap] = React.useState(null)
-
-//   const onLoad = React.useCallback(function callback(map:any) {
-//     // This is just an example of getting and using the map instance!!! don't just blindly copy!
-//     const bounds = new window.google.maps.LatLngBounds(center);
-//     map.fitBounds(bounds);
-
-//     setMap(map)
-//   }, [])
-
-//   const onUnmount = React.useCallback(function callback() {
-//     setMap(null)
-//   }, [])
-
-//   return isLoaded ? (
-//       <GoogleMap
-//         mapContainerStyle={containerStyle}
-//         center={center}
-//         zoom={10}
-//         onLoad={onLoad}
-//         onUnmount={onUnmount}
-//       >
-//         { /* Child components, such as markers, info windows, etc. */ }
-//         <></>
-//       </GoogleMap>
-//   ) : <></>
-// }
-
-// export default React.memo(MyComponent)
