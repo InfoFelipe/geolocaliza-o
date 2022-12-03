9d6df03783a508319575816933ee84099b80a901
Felipe Antonio <96073664+InfoFelipe@users.noreply.github.com>
2022-12-02 22:05:50 -0300

    Organização do código
    Organização do código

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,3 @@
-
-
 import React from 'react';
 import { Card } from 'primereact/card';
 import { InputText } from 'primereact/inputText';
@@ -51,7 +49,9 @@ const Form: NextPage = () => {
     return (
     <>
     <Container>
-    <Card style={{marginTop:'5%', padding:'auto', width:'40%', marginLeft:'30%', backgroundColor:'#2d3b45', border:'2px solid #202020'}} >
+    <Card 
+    style={{marginTop:'5%', padding:'auto', width:'40%', 
+    marginLeft:'30%', backgroundColor:'#2d3b45', border:'2px solid #202020'}} >
     <div className='container'>
     <div className='row'>
     <div className='col'>
@@ -117,67 +117,8 @@ const Form: NextPage = () => {
 
           <Mapeamento />               
     </Card>
-        
     
     </Container>
     </>
     );
 }
-export default Form
-// import {Menubar} from 'primereact/menubar'
-// import { Card } from 'primereact/card';
-// import { Button } from 'primereact/button';
-// import { NextPage } from 'next';
-
-// const Home: NextPage = () =>{
-
-//   const itens = [
-//     {
-//       label: "Home",
-//       icon:"pi pi-home"
-//     },
-//     {
-//       label: "Eng. Software",
-//       icon:"pi pi-cog"
-//     },
-//     {
-//       label: "Front-End",
-//       icon:"pi pi-code"
-//     },
-//     {
-//       label: "Back-End",
-//       icon:"pi pi-code"
-//     },
-//     {
-//       label: "Banco de Dados",
-//       icon:"pi pi-database"
-//     },
-//     {
-//       label: "Discord",
-//       icon:"pi pi-discord px-2"
-//     },
-//     {
-//       label: "Youtube",
-//       icon:"pi pi-youtube px-2"
-//     },
-//     {
-//       label: "Instagram",
-//       icon:"pi pi-instagram"
-//     }
-
-//   ]
-
- 
-
-//   return(
-//     <>
-   
-//       <Menubar model={itens}/>
-//   <Card style={{padding:'0px', borderRadius:'0px', alignItems:'center', backgroundColor:'#cae6fc'}}>
-//     <h1 style={{ position:'relative', marginLeft:'auto'}}>Venha fazer parte do mundo da Tecnologia</h1>
-//     <Button>Inscreva-se</Button>
-//   </Card>
-//     </>
-//   )
-// }
-// export default Home
